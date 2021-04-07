a3af80d967806095f112c133eeaebb00105b3704
fAlfaro21 <falfarogr@hotmail.com>
2021-04-07 10:27:25 +0200

    updated db.js

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,19 +1,19 @@
 
 require('dotenv').config()
 const mysql      = require('mysql');
-const connection = mysql.createConnection({
+const dbConnection = mysql.createConnection({
   host     : process.env.DB_HOST,
   user     : process.env.DB_USER,
   password : process.env.DB_PASS
 });
  
-connection.connect(function(err) {
+dbConnection.connect(function(err) {
   if (err) {
     console.error('error connecting: ' + err.stack);
     return;
   }
  
-  console.log('connected as id ' + connection.threadId);
+  console.log('connected as id ' + dbConnection.threadId);
 });
 
-module.exports = connection;
+module.exports = dbConnection;
