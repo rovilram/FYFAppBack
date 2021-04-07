3478b2bda239715b96b2b459fccb23dc0199aa7b
fAlfaro21 <falfarogr@hotmail.com>
2021-04-07 09:03:30 +0200

    Updated db.js

--- src/config/db.js
+++ src/config/db.js
@@ -2,12 +2,18 @@
 const express = require("express");
 const mysql      = require('mysql');
 const connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'me',
-  password : 'secret',
-  database : 'my_db'
+  host     : 'example.org',
+  user     : 'bob',
+  password : 'secret'
 });
  
-connection.connect();
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+ 
+  console.log('connected as id ' + connection.threadId);
+});
 
 module.exports = connection;
