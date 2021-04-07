adec536e6d8a09fc40894e51304212efcaee192d
fAlfaro21 <falfarogr@hotmail.com>
2021-04-07 10:16:01 +0200

    updated db.js

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,9 +2,9 @@
 require('dotenv').config()
 const mysql      = require('mysql');
 const connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'equipo',
-  password : 'secret'
+  host     : process.env.DB_HOST,
+  user     : process.env.DB_USER,
+  password : process.env.DB_PASS
 });
  
 connection.connect(function(err) {
