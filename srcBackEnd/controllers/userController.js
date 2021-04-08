fdc30161782a0eeb4d4368595c1d3c341ee2ecb7
fAlfaro21 <falfarogr@hotmail.com>
2021-04-08 19:41:49 +0200

    Initial version for updateUser endpoint

--- srcBackEnd/controllers/userController.js
+++ srcBackEnd/controllers/userController.js
@@ -1,7 +1,87 @@
 exports.getUser = () => {};
 
-exports.updateUser = () => {
-    
-};
+exports.updateUser = async (req, res) => {
+
+    // **/user** _PATCH_ (TOKEN AUTH) Modifica alguno de los datos de perfil del usuario.
+
+    const user = req.body.id;
+    const nombre = req.body.nombre;
+    const apellidos = req.body.apellidos;
+    const foto = req.body.foto;
+  
+    //Validamos los campos user y password
+    if (isValidUserPass(user, password, res)) {
+      //generamos una clave secreta para el JWT del usuario
+      const secret = nanoid();
+        
+        if (nombre) {
+                try {
+                    const response = await dbConnection.query("UPDATE TABLE SET nombre = ?" , [nombre]);
+                    res.send({
+                    OK: 1,
+                    message: 'Profile updated'
+                    });
+                } catch (error) {
+                    if (error.code === 11000) {
+                    res.status(409).send({
+                        OK: 0,
+                        error: 409,
+                        message: error.message,
+                    });
+                    }
+                    res.status(500).send({
+                    OK: 0,
+                    error: 500,
+                    message: error.message,
+                    });
+                }
+        }
+        else if (apellidos) {
+            try {
+                const response = await dbConnection.query("UPDATE TABLE SET apellidos = ?" , [apellidos]);
+                res.send({
+                OK: 1,
+                message: 'Profile updated'
+                });
+            } catch (error) {
+                if (error.code === 11000) {
+                res.status(409).send({
+                    OK: 0,
+                    error: 409,
+                    message: error.message,
+                });
+                }
+                res.status(500).send({
+                OK: 0,
+                error: 500,
+                message: error.message,
+                });
+            }
+        }
+        else if (foto) {
+            try {
+                const response = await dbConnection.query("UPDATE TABLE SET foto = ?" , [foto]);
+                res.send({
+                OK: 1,
+                message: 'Profile updated'
+                });
+            } catch (error) {
+                if (error.code === 11000) {
+                res.status(409).send({
+                    OK: 0,
+                    error: 409,
+                    message: error.message,
+                });
+                }
+                res.status(500).send({
+                OK: 0,
+                error: 500,
+                message: error.message,
+                });
+            }
+        }
+    }
+  };
+
 
 exports.deleteUser = () => {};
